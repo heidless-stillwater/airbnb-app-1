0ae3737054dccbdd2e1d4dd13c7d727e877405ff
heidless <lockhart.r@gmail.com>
2024-09-30 19:39:22 +0100

    init

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,26 @@
+
+
+require 'rails_helper'
+
+RSpec.describe "Api::Users", type: :request do 
+  describe "GET show" do 
+    let(:headers) do 
+      { "ACCEPT" => "application/json" }
+    end
+    context "user exists" do 
+      it "is successful" do 
+        user = create(:user)
+        get api_user_path(id: user), headers: headers
+        expect(response).to be_successful
+      end
+    end
+
+    context "user does not exist" do 
+      it "is not found" do 
+        get api_user_path(id: "junk"), headers: headers
+        # expect(response).not_to be_successful     # WORKS
+        expect(response.status).to eq 404           # WORKS
+      end
+    end
+  end
+end
