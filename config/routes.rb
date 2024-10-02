e17a843c48b827ad9fb716445d753d0cae054240
heidless <lockhart.r@gmail.com>
2024-10-02 20:19:59 +0100

    removed duplicate call from footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  get "pages/index"
+  get "pages/show"
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
